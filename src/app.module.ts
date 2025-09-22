72ec5665036828ac3a6027e8e7bc86bd1d4500d8
Diego Bustos <diecode.bal@outlook.com>
2025-09-21 22:21:25 -0600

    feat: update AppModule to include InfrastructureModule and ApplicationModule for improved modularity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { ApplicationModule } from './application/application.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [InfrastructureModule, ApplicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
