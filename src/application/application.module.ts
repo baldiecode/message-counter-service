b39e399f9edf0885f0c5f0fecae8f3575cb3bceb
Diego Bustos <diecode.bal@outlook.com>
2025-09-22 15:13:38 -0600

    fix(application): wire domain module and align query handler with repository interface

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { DomainModule } from '../domain/domain.module';
+import { InfrastructureModule } from '../infrastructure/infrastructure.module';
 import { ProcessWebhookHandler } from './commands/process-webhook.handler';
 import { GetCountsHandler } from './queries/get-counts.handler';
-import { DomainModule } from '../domain/domain.module';
 
 const CommandHandlers = [ProcessWebhookHandler];
 const QueryHandlers = [GetCountsHandler];
 
 @Module({
-  imports: [CqrsModule, DomainModule],
+  imports: [CqrsModule, InfrastructureModule, DomainModule],
   providers: [...CommandHandlers, ...QueryHandlers],
   exports: [...CommandHandlers, ...QueryHandlers],
 })
